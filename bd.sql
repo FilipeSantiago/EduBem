45cedd0a00cbc271738e996b3b932b6f069cd83c
FilipeSantiago <filipe_santiago@id.uff.br>
2015-03-23 17:30:42 -0300

    Primeira Versão do Banco de Dados Ps.: MySQL

---
+++ bd.sql
@@ -0,0 +1,27 @@
+
+create table Usuario(
+	idUsuario int ;
+	nome varchar(100);
+	email varchar(100);
+	foto MEDIUMBLOB;
+	cadastro date;
+	primary Key(idUsuario);
+);
+
+create table Post(
+	idPost int;
+	titulo varchar(100);
+	corpo varchar(1000);
+	postado date;
+	idUsuario int;
+	primary Key(idPost);
+	FOREIGN KEY (idUsuario) REFERENCES Usuario (idUsuario));
+);
+
+
+create table Tags(
+	idPost int;
+	tag varchar(20);
+	primary Key(idPost, tag);
+	FOREIGN KEY (idPost) REFERENCES Post (idPost));
+);
